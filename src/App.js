7ffee36c96886d7639928aedb20d40d3579b1f75
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-09-15 20:06:18 +0200

    linked reg form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import HomePage from "./pages/HomePage"
 import Registrstion from "./pages/Registration";
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import { HashRouter, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename={"/"}>
+    <HashRouter basename="/">
       <Routes>
         {/* The Router decides which component to show based on the current URL.*/}
-        <Route exact path="" element={<HomePage/>}></Route>
-        <Route exact path="/signup" element={<Registrstion/>}></Route>
+        <Route exact path="/" element={<HomePage/>}></Route>
+        {/* <Route exact path="/signup" element={<Registrstion/>}></Route> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default App;
